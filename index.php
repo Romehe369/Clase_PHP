cc575bc658fad2744a3bbcd76f887033887587d8
Ronald Huahuatico Soria <69460625+Romehe369@users.noreply.github.com>
2022-08-29 00:23:46 -0500

    Modificacion del index
    Se ha modificado el index con la utilizacion de nuevas funciones

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php 
-    include 'AllFunctions.php';
-    $Mostrar=[];
-    $Alumnos_No_Matriculados=[];
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,102 +6,128 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <title>Examen Matriculados</title>
+    <title>Matriculados</title>
 </head>
 <body>
     <div class="container">
-        <h3 class="pb-3 pt-5">Agregar Archivos</h3>
-        <!-- Creamos un formulario para leer los archivos -->
-        <form action="index.php" method="post" enctype="multipart/form-data">
-            <label for="formFile" class="form-label">Matriculados General 2022 (.csv)</label>
-            <!-- Leemos docentes, en nombre Docentes -->
-            <input class="form-control mb-3" name="Matriculados_General" type="file" id="formFile">
-
-            <label for="formFile" class="form-label">Docentes 2022 (.csv)</label>
-            <!-- Leemos alumnos Matriculados General 2022, en nombre Matriculados_General -->
-            <input class="form-control mb-3" name="Docentes" type="file" id="formFile">
-
-            <label for="formFile" class="form-label">Distribución docente de tutorias en 2021-1 (.csv)</label>
-            <!-- Leemos alumnos Distribución docente de tutorias en 2021-1, en nombre archivo1 -->
-            <input class="form-control mb-3" name="Distribucion_docente" type="file" id="formFile">
-            <input type="submit" class="btn btn-primary" value="Cargar y exportar">
-        </form>
-        <?php
-            #Leemos los archivos
-            if (!isset($_FILES["Matriculados_General"])) {
-                #throw new Exception("Selecciona un archivo CSV válido");
-            }
-            if (!isset($_FILES["Distribucion_docente"])) {
-                #throw new Exception("Selecciona un archivo CSV válido.");
-            }
-            else{
-                #Cada archivo sera leido
-                $Matriculados_General   = $_FILES["Matriculados_General"];
-                $Distribucion_docente    = $_FILES["Distribucion_docente"];
-                #Creamo un objeto
-                $Mox=new Group73();
-                #Obtenemos los matriculados en genereal
-                $Arreglo_Matriculados=$Mox->csv_Array($Matriculados_General,0);
-                #Obtenemos la de alumnos con tutor del anterior semestre
-                $Arreglo_Dis_Docentes=$Mox->csv_Array($Distribucion_docente,0);
-                #$Mox->Imprimir($Arreglo_Dis_Docentes);
-                #Obtenemos el alumno y el docente del anterior semestre
-                $Arreglo_Dis_Docentes_Anterior=$Mox->csv_Array($Distribucion_docente,3);
-                #Obtenemos los alumnos no matriculados
-                $Alumnos_No_Matriculados=$Mox->Diferencia($Arreglo_Dis_Docentes,$Arreglo_Matriculados);
-                #$Mox->Imprimir($Arreglo_Matriculados);
-                #Obtenemos los alumnos sin tutor
-                $AlumnosSinTutor=$Mox->Diferencia($Arreglo_Matriculados,$Arreglo_Dis_Docentes);
-                $Mostrar=$AlumnosSinTutor;
-            ?>
-        <p>
-        <h6> EXPORTAR:LOS DATOS SE GUARDAN EN:</p>
-        <ul>
-            <li>\xampp\htdocs\"Nombre de la carpeta"\No_Considerados_En_Tutoria.csv<br></li>
-            <li>\xampp\htdocs\"Nombre de la carpeta"\Distribucion_balanceada_tutorias.csv<br></li></ul>
-        </h6>
-        <p>
-            <b>Aqui podemos vizualizar los datos de los alumnos sin tutor:</b>
-        <p>
-        <div class="row"> 
-            <div class="tab-pane container" id="Ejecutar">
-                <table class="table" >
-                    <thead class="table-success table-striped" id="Ejecutar" >
-                        <th>Numero</th>
-                        <th>Codigo</th>
-                        <th>Nombre</th>
-                    </thead>
-                    <tbody>
-                        <?php  
-                            $Mox->Imprimir($AlumnosSinTutor);
-                            $Balanceado=$Mox->Balancear($AlumnosSinTutor,$AlumnosSinTutor,$Arreglo_Dis_Docentes_Anterior,$Alumnos_No_Matriculados);
-                            $Mox->GenerarCSV_No_Considerados($Alumnos_No_Matriculados);
-                            #$Mox->GenerarCSV($Alumnos_No_Matriculados);
-                            $Mox->GenerarCSV_Distribucion($Balanceado);
-                        
-                        ?>
-                    </tbody>
-                </table>
+        <div class="row">
+            <h3 class="pb-3 pt-5">Agregar Archivos</h3>
+            <!-- Para Agregar CSV -->
+            <div class="col-12 pb-3">
+                <div class="mb-3">
+                    <form action="index.php" method="post" enctype="multipart/form-data">
+                            <label for="formFile" class="form-label">Alumnos Matriculados General 2022 (.csv)</label>
+                            <input class="form-control mb-3" name="FileAlumnos" type="file" id="formFile">
+                            
+                            <label for="formFile" class="form-label">Distribución docente de tutorias 2021-1 (.csv)</label>
+                            <input class="form-control mb-3" name="FileDistribucion" type="file" id="formFile">
+                            
+                            <label for="formFile" class="form-label">Docente 2022 (.csv)</label>
+                            <input class="form-control mb-3" name="FileDocente" type="file" id="formFile">
+                            
+                            <input type="submit" class="btn btn-primary" value="Cargar">
+                    </form>
+                </div>
             </div>
-        </div>    
-        <div class="row"> 
-            <div class="tab-pane container">
-                <p><h5> Resultados de la distribución balanceada de tutorias para el presente semestre 2022-I</h5></p>
-                <table class="table" >
-                    <thead class="table-success table-striped" id="Ejecutar" >
-                        <th>Numero</th>
-                        <th>Codigo</th>
-                        <th>Nombre</th>
-                    </thead>
-                    <tbody>
-                        <?php  
-                            $Mox->Imprimir($Balanceado);
+        </div>
+    </div>
+    <?php
+        /* Iniciar Valores */
+        include 'AllFunctions.php';
+        if (!isset($_FILES["FileAlumnos"]) or !isset($_FILES["FileDistribucion"]) or !isset($_FILES["FileDocente"])) {
+            throw new Exception("Selecciona un archivo CSV válido.");
+        }
+        $FileAlumnos          = $_FILES["FileAlumnos"];
+        $FileDistribucion     = $_FILES["FileDistribucion"];
+        $FileDocente          = $_FILES["FileDocente"];
+        
+        $ControlMox=new AllFunctions();
+        $ArrAlumnos2022=$ControlMox->csv_to_ArrayGeneral($FileAlumnos);
+        $ArrDocente2022=$ControlMox->csv_to_ArrayGeneral($FileDocente);
+        #Arreglo de matriculas -----Estan los alumnos antiguos
+        $ArrMatriculas2021=$ControlMox->csv_to_ArrayGeneral($FileDistribucion);
+        #Arreglo general de alumnos antiguos
+        $Total_Alumnos_Antiguos=$ControlMox->Lista_Alumnos($ArrMatriculas2021);
+        #Obetenemos los alumnos nuevos
+        $Arr_Nuevos=$ControlMox->Diferencia($ArrAlumnos2022,$Total_Alumnos_Antiguos);
+        #Obtenemos los no matriculados
+        $Arr_no_matriculados=$ControlMox->Diferencia($Total_Alumnos_Antiguos,$ArrAlumnos2022);
+        #$Arr_No_Matriculados=$ControlMox->Diferencia($ArrMatriculas2021,$ArrAlumnos2022,2);
+        $ControlMox->Borrar_Alumnos_No_Matriculados($ArrMatriculas2021,$Arr_no_matriculados);
+        $ControlMox->Balancear($ArrMatriculas2021,$Arr_Nuevos);
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="pb-3">Tablas</h3>
+                <!-- Nav tabs -->
+                <ul class="nav nav-tabs pb-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-bs-toggle="tab" href="#DxD">Tabla balanceada</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="tab" href="#Alumno">Alumnos 2022</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="tab" href="#Docente">Docentes 2022</a>
+                    </li>
+                </ul>
+
+                <div class="tab-content p-3">
+                    <div class="tab-pane container active" id="DxD">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Codigo</th>
+                                <th>Nombre</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            if(!empty($ArrMatriculas2021)){
+                                $ControlMox->ImprimirTablaMatricula($ArrMatriculas2021);
                             }
-                        ?>
-                    </tbody>
-                </table>
+                            ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="tab-pane container" id="Alumno">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Codigo</th>
+                                <th>Nombre</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            if(!empty($ArrAlumnos2022)){
+                                $ControlMox->ImprimirTabla($ArrAlumnos2022);
+                            }
+                            ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="tab-pane container" id="Docente">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Categoria</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            if(!empty($ArrDocente2022)){
+                                $ControlMox->ImprimirTabla($ArrDocente2022);
+                            }
+                            ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-        </div>            
+        </div>
     </div>
 </body>
 </html>
